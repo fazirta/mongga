5c45a8a26dd27968cbcc8db16b7ec1063cfe552d
naufala <ahmadajinaufalali@gmail.com>
2021-09-25 23:11:18 +0700

    fix navbar logo

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -1,105 +1,127 @@
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
 import {
-    BookmarkAltIcon,
-    CalendarIcon,
-    MenuIcon,
-    PhoneIcon,
-    AcademicCapIcon,
-    ShieldCheckIcon,
-    SupportIcon,
-    UsersIcon,
-    XIcon,
-    UserGroupIcon,
-    ArchiveIcon,
-} from '@heroicons/react/outline'
-import {
-    a,
-} from "react-router-dom";
+  BookmarkAltIcon,
+  CalendarIcon,
+  MenuIcon,
+  PhoneIcon,
+  AcademicCapIcon,
+  ShieldCheckIcon,
+  SupportIcon,
+  UsersIcon,
+  XIcon,
+  UserGroupIcon,
+  ArchiveIcon,
+} from "@heroicons/react/outline";
+import { a } from "react-router-dom";
 
 const solutions = [
-    {
-        name: 'Tentang Kami',
-        description: 'Get a better understanding of where your traffic is coming from.',
-        href: '/aboutus',
-        icon: UsersIcon,
-    },
-    {
-        name: 'Aktivitas',
-        description: 'Speak directly href your customers in a more meaningful way.',
-        href: '/aktivitas',
-        icon: AcademicCapIcon,
-    },
-    { name: 'Produk', description: "Your customers' data will be safe and secure.", href: '/produk', icon: ArchiveIcon },
-    {
-        name: 'Komunitas',
-        description: "Connect with third-party tools that you're already using.",
-        href: '/komunitas',
-        icon: UserGroupIcon,
-    },
-    {
-        name: 'Hubungi Kami',
-        description: 'Build strategic funnels that will drive your customers href convert',
-        href: '/contactus',
-        icon: PhoneIcon,
-    },
-]
+  {
+    name: "Tentang Kami",
+    description:
+      "Get a better understanding of where your traffic is coming from.",
+    href: "/aboutus",
+    icon: UsersIcon,
+  },
+  {
+    name: "Aktivitas",
+    description: "Speak directly href your customers in a more meaningful way.",
+    href: "/aktivitas",
+    icon: AcademicCapIcon,
+  },
+  {
+    name: "Produk",
+    description: "Your customers' data will be safe and secure.",
+    href: "/produk",
+    icon: ArchiveIcon,
+  },
+  {
+    name: "Komunitas",
+    description: "Connect with third-party tools that you're already using.",
+    href: "/komunitas",
+    icon: UserGroupIcon,
+  },
+  {
+    name: "Hubungi Kami",
+    description:
+      "Build strategic funnels that will drive your customers href convert",
+    href: "/contactus",
+    icon: PhoneIcon,
+  },
+];
 const resources = [
-    {
-        name: 'Help Center',
-        description: 'Get all of your questions answered in our forums or contact support.',
-        href: '#',
-        icon: SupportIcon,
-    },
-    {
-        name: 'Guides',
-        description: 'Learn how href maximize our platform href get the most out of it.',
-        href: '#',
-        icon: BookmarkAltIcon,
-    },
-    {
-        name: 'Events',
-        description: 'See what meet-ups and other events we might be planning near you.',
-        href: '#',
-        icon: CalendarIcon,
-    },
-    { name: 'Security', description: 'Understand how we take your privacy seriously.', href: '#', icon: ShieldCheckIcon },
-]
+  {
+    name: "Help Center",
+    description:
+      "Get all of your questions answered in our forums or contact support.",
+    href: "#",
+    icon: SupportIcon,
+  },
+  {
+    name: "Guides",
+    description:
+      "Learn how href maximize our platform href get the most out of it.",
+    href: "#",
+    icon: BookmarkAltIcon,
+  },
+  {
+    name: "Events",
+    description:
+      "See what meet-ups and other events we might be planning near you.",
+    href: "#",
+    icon: CalendarIcon,
+  },
+  {
+    name: "Security",
+    description: "Understand how we take your privacy seriously.",
+    href: "#",
+    icon: ShieldCheckIcon,
+  },
+];
 
 export default function Navbar() {
-    return (
-        <Popover className="z-50 sticky top-0 relative bg-white border-b-2 border-gray-100 font-barlow">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6">
-                <div className="flex justify-between items-center py-6 px-10 lg:px-20 md:justify-between">
-                    <div className="flex justify-start">
-                        <a href="/">
-                            <span className="sr-only">Mongga</span>
-                            <img
-                                className="h-8 w-auto sm:h-10"
-                                src="https://mongga.id/wp-content/uploads/2021/02/cropped-MONGGA-LOGO-P1.png"
-                                alt="Mongga"
-                            />
-                        </a>
-                    </div>
-                    <div className="-mr-2 -my-2 md:hidden">
-                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
-                            <span className="sr-only">Open menu</span>
-                            <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                        </Popover.Button>
-                    </div>
-                    <Popover.Group as="nav" className="hidden md:flex space-x-16 px-10">
-                        <a href="/aboutus" className="text-base font-medium text-gray-500 hover:text-gray-900">
-                            Tentang Kami
-                        </a>
-                        <a href="/aktivitas" className="text-base font-medium text-gray-500 hover:text-gray-900">
-                            Aktivitas
-                        </a>
-                        
-                        <a href="/produk" className="text-base font-medium text-gray-500 hover:text-gray-900">
-                            Produk
-                        </a>
+  return (
+    <Popover className="z-50 sticky top-0 relative bg-white border-b-2 border-gray-100 font-barlow">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center py-6 px-10 lg:px-20 md:justify-between">
+          <div className="flex justify-start">
+            <a href="/">
+              <span className="sr-only">Mongga</span>
+              <img
+                className="h-10 w-auto md:h-0 lg:h-10"
+                src="https://mongga.id/wp-content/uploads/2021/02/cropped-MONGGA-LOGO-P1.png"
+                alt="Mongga"
+              />
+            </a>
+          </div>
+          <div className="-mr-2 -my-2 md:hidden">
+            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
+              <span className="sr-only">Open menu</span>
+              <MenuIcon className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+          <Popover.Group as="nav" className="hidden md:flex space-x-16 px-10">
+            <a
+              href="/aboutus"
+              className="text-base font-medium text-gray-500 hover:text-gray-900"
+            >
+              Tentang Kami
+            </a>
+            <a
+              href="/aktivitas"
+              className="text-base font-medium text-gray-500 hover:text-gray-900"
+            >
+              Aktivitas
+            </a>
 
-                        {/* <Popover className="relative">
+            <a
+              href="/produk"
+              className="text-base font-medium text-gray-500 hover:text-gray-900"
+            >
+              Produk
+            </a>
+
+            {/* <Popover className="relative">
                             {({ open }) => (
                                 <>
                                     <Popover.Button
@@ -163,12 +185,15 @@ export default function Navbar() {
                                 </>
                             )}
                         </Popover> */}
-                        
-                        <a href="/komunitas" className="text-base font-medium text-gray-500 hover:text-gray-900">
-                            Komunitas
-                        </a>
 
-                        {/* <Popover className="relative">
+            <a
+              href="/komunitas"
+              className="text-base font-medium text-gray-500 hover:text-gray-900"
+            >
+              Komunitas
+            </a>
+
+            {/* <Popover className="relative">
                             {({ open }) => (
                                 <>
                                     <Popover.Button
@@ -239,78 +264,95 @@ export default function Navbar() {
                                 </>
                             )}
                         </Popover> */}
-                        <a href="/contactus" className="text-base font-medium text-gray-500 hover:text-gray-900">
-                            Hubungi Kami
-                        </a>
-                    </Popover.Group>
-                </div>
-            </div>
-
-            <Transition
-                as={Fragment}
-                enter="duration-200 ease-out"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="duration-100 ease-in"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
+            <a
+              href="/contactus"
+              className="text-base font-medium text-gray-500 hover:text-gray-900"
             >
-                <Popover.Panel focus className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-                    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-                        <div className="pt-5 pb-6 px-5">
-                            <div className="flex items-center justify-between">
-                                <div>
-                                    <img
-                                        className="h-8 w-auto"
-                                        src="https://mongga.id/wp-content/uploads/2021/02/cropped-MONGGA-LOGO-P1.png"
-                                        alt="Mongga"
-                                    />
-                                </div>
-                                <div className="-mr-2">
-                                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
-                                        <span className="sr-only">Close menu</span>
-                                        <XIcon className="h-6 w-6" aria-hidden="true" />
-                                    </Popover.Button>
-                                </div>
-                            </div>
-                            <div className="mt-6">
-                                <nav className="grid gap-y-8">
-                                    {solutions.map((item) => (
-                                        <a
-                                            key={item.name}
-                                            href={item.href}
-                                            className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                                        >
-                                            <item.icon className="flex-shrink-0 h-6 w-6 text-green-600" aria-hidden="true" />
-                                            <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
-                                        </a>
-                                    ))}
-                                </nav>
-                            </div>
-                        </div>
-                        <div className="py-6 px-5 space-y-6">
-                            <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
-                                    Pricing
-                                </a>
+              Hubungi Kami
+            </a>
+          </Popover.Group>
+        </div>
+      </div>
 
-                                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
-                                    Docs
-                                </a>
-                                {resources.map((item) => (
-                                    <a
-                                        key={item.name}
-                                        href={item.href}
-                                        className="text-base font-medium text-gray-900 hover:text-gray-700"
-                                    >
-                                        {item.name}
-                                    </a>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </Popover.Panel>
-            </Transition>
-        </Popover>
-    )
+      <Transition
+        as={Fragment}
+        enter="duration-200 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel
+          focus
+          className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+        >
+          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+            <div className="pt-5 pb-6 px-5">
+              <div className="flex items-center justify-between">
+                <div>
+                  <img
+                    className="h-8 w-auto"
+                    src="https://mongga.id/wp-content/uploads/2021/02/cropped-MONGGA-LOGO-P1.png"
+                    alt="Mongga"
+                  />
+                </div>
+                <div className="-mr-2">
+                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
+                    <span className="sr-only">Close menu</span>
+                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+              <div className="mt-6">
+                <nav className="grid gap-y-8">
+                  {solutions.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                    >
+                      <item.icon
+                        className="flex-shrink-0 h-6 w-6 text-green-600"
+                        aria-hidden="true"
+                      />
+                      <span className="ml-3 text-base font-medium text-gray-900">
+                        {item.name}
+                      </span>
+                    </a>
+                  ))}
+                </nav>
+              </div>
+            </div>
+            <div className="py-6 px-5 space-y-6">
+              <div className="grid grid-cols-2 gap-y-4 gap-x-8">
+                <a
+                  href="#"
+                  className="text-base font-medium text-gray-900 hover:text-gray-700"
+                >
+                  Pricing
+                </a>
+
+                <a
+                  href="#"
+                  className="text-base font-medium text-gray-900 hover:text-gray-700"
+                >
+                  Docs
+                </a>
+                {resources.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
+  );
 }
