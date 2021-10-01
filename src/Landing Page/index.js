0400e880218803c7d1ef63153ef8c703c1013487
Fazil <faziltirtana@ctemplar.com>
2021-10-01 11:27:42 +0700

    Slicing landing page(benerin card biar responsive)

--- src/Landing Page/index.js
+++ src/Landing Page/index.js
@@ -3,25 +3,28 @@ import CardPersegi from '../Card Persegi';
 import CardPersegiPanjang from '../Card Persegi Panjang';
 import Jumbotron from '../Jumbotron';
 import hero from '../img/NEWLAND 6.png';
+import online from '../img/online gr 1.png';
+import leader from '../img/leader 1.png';
+import crowd from '../img/crowd 1.png';
+import group193 from '../img/Group 193.png';
 
 export default function LandingPage() {
     return (
         <>
             <Jumbotron href="/produk" src={hero} />
             <div className="px-3 py-5 xl:py-10 xl:px-0 xl:max-w-7xl mx-auto">
-                <div className="flex space-x-5 my-10">
-                    <ButtonHijau href="/contactus" text="Hubungi Kami" />
-                    <ButtonHijau href="/contactus" text="Pelajari Selengkapnya" />
+                <div className="mt-5 md:mt-10">
+                    <div className="md:px-32 lg:px-72">
+                        <h1 className="font-bold text-3xl lg:text-4xl text-darkgreen text-center font-poppins">Asah kemampuan dan kualitas diri bersama Mongga!</h1>
+                    </div>
+                    <div className="flex flex-col lg:flex-row space-y-5 md:space-y-10 lg:space-y-0 justify-around mt-10 lg:px-10">
+                        <CardPersegiPanjang title="Penyedia layanan Pelatihan dan Konsultasi yang Terpercaya dan Kompeten" link="Aktivitas Kami >" src={online} href="/aktivitas" />
+                        <CardPersegiPanjang title="Mentor Berpengalaman dan Berkualitas di bidangnya" link="Mentor Kami >" src={leader} href="/aktivitas" />
+                        <CardPersegiPanjang title="Telah digunakan dan dibuktikan oleh banyak orang" link="Testimoni >" src={crowd} href="/aktivitas" />
+                    </div>
                 </div>
-                <div className="flex justify-between my-10">
-                    <CardPersegi title="Olimpiade" src="https://www.sisega.com.mx/wp-content/uploads/2016/08/ef3-placeholder-image.jpg" href="/produk" />
-                    <CardPersegi title="Riset" src="https://www.sisega.com.mx/wp-content/uploads/2016/08/ef3-placeholder-image.jpg" href="/produk" />
-                    <CardPersegi title="Bahasa" src="https://www.sisega.com.mx/wp-content/uploads/2016/08/ef3-placeholder-image.jpg" href="/produk" />
-                </div>
-                <div className="flex justify-between my-10">
-                    <CardPersegiPanjang title="Penyedia layanan Pelatihan dan Konsultasi yang Terpercaya dan Kompeten" link="Aktivitas Kami >" src="https://www.sisega.com.mx/wp-content/uploads/2016/08/ef3-placeholder-image.jpg" href="/aktivitas" />
-                    <CardPersegiPanjang title="Mentor Berpengalaman dan Berkualitas di bidangnya" link="Mentor Kami >" src="https://www.sisega.com.mx/wp-content/uploads/2016/08/ef3-placeholder-image.jpg" href="/aktivitas" />
-                    <CardPersegiPanjang title="Telah digunakan dan dibuktikan oleh banyak orang" link="Testimoni >" src="https://www.sisega.com.mx/wp-content/uploads/2016/08/ef3-placeholder-image.jpg" href="/aktivitas" />
+                <div className="mt-10 md:mt-20">
+                    <img src={group193} className="max-h-20 md:max-h-24 mx-auto" />
                 </div>
             </div>
         </>
