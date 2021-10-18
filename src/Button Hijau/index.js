2289ad5ff076768a45201723cf9449c2d1d8ee3d
Fazil Tirtana <faziltirtana@ctemplar.com>
2021-10-18 09:16:56 +0700

    Perbaiki animasi saat hover pada card dan button

--- src/Button Hijau/index.js
+++ src/Button Hijau/index.js
@@ -7,7 +7,7 @@ export default function ButhrefnHijau(props) {
         <>
             <div className="flex justify-center">
                 <a href={props.href}>
-                    <div className="shadow-xl hover:shadow-lg transition ease-out duration-200 transform hover:-translate-y-0.5 hover:scale-105 inline-block bg-darkgreen rounded-lg font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-green-900 font-poppins shadow-2xl">
+                    <div className="shadow-lg hover:shadow-xl transition ease-out duration-200 transform hover:-translate-y-0.5 hover:scale-105 inline-block bg-darkgreen rounded-lg font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-green-900 font-poppins shadow-2xl">
                         {props.text}
                     </div>
                 </a>
--- src/Card Persegi Panjang/index.js
+++ src/Card Persegi Panjang/index.js
@@ -6,7 +6,7 @@ export default function CardPersegiPanjang(props) {
     return (
         <>
             <a href={props.href} className="CardPersegiPanjang">
-                <div className="flex flex-row lg:flex-col space-x-4 lg:space-x-0 p-4 lg:p-0 justify-between shadow-xl hover:shadow-lg transition ease-out duration-200 transform hover:-translate-y-0.5 hover:scale-105 max-w-xl mx-auto lg:w-72 lg:h-full rounded-xl font-poppins" style={{ backgroundColor: '#F8F8F8' }}>
+                <div className="flex flex-row lg:flex-col space-x-4 lg:space-x-0 p-4 lg:p-0 justify-between shadow-lg hover:shadow-xl transition ease-out duration-200 transform hover:-translate-y-0.5 hover:scale-105 max-w-xl mx-auto lg:w-72 lg:h-full rounded-xl font-poppins" style={{ backgroundColor: '#F8F8F8' }}>
                     <div className="flex flex-col lg:flex-row justify-center align-center px-2 md:px-5 lg:px-0 lg:py-8">
                         <img alt={props.text} className="w-auto h-16 lg:w-auto md:h-28 lg:h-auto" src={props.src} />
                     </div>
