9e49e841d079e6ca8eae623e11570cb3e02654e1
jeegaje <anggajiyanfajar@gmail.com>
2021-11-01 07:27:35 +0700

    Update ContactUs

--- src/Contact Us/index.js
+++ src/Contact Us/index.js
@@ -1,6 +1,10 @@
 import ButtonHijau from "../Button Hijau";
 import JumbotronHijau from "../Jumbotron Hijau";
 import Heading1 from "../Heading1";
+import ContactUsLogo from "../img/contactus-logo.png";
+import Instagram from "../img/Instagram_white 1.png";
+import Linkedin from "../img/LinkedIN_white 1.png";
+import Facebook from "../img/Facebook_white 1.png";
 
 export default function ContactUs() {
   return (
@@ -22,7 +26,7 @@ export default function ContactUs() {
             atau hubungi kami melalui{" "}
             <a href="mailto:support@mongga.id">&nbsp; support@mongga.id</a>
           </p>
-          <div className="flex items-center justify-center py-10">
+          <div className="flex items-center justify-center py-10 my-10">
             <div className="border-4 rounded-full w-4 h-4 bg-gray-400 border-gray-400"></div>
             <div className="px-2"></div>
             <div className="border-4 rounded-full w-4 h-4 bg-gray-400 border-gray-400"></div>
@@ -37,6 +41,39 @@ export default function ContactUs() {
           <div className="flex items-center justify-center text-sm py-6">
             <ButtonHijau href="https://t.me/joinchat/-FYL91EUszBmZGQ1" text="Gabung di telegram" />
           </div>
+          <div className="flex items-center justify-center text-sm py-6">
+            <div className="flex justify-center">
+              <div className="bg-darkgreen rounded-large px-4 py-3 transition duration-300 ease-in-out font-poppins shadow-2xl">
+                <div class="flex flex-col lg:flex-row items-center content-center px-7">
+                  <img className="m-5 lg:mr-10" src={ContactUsLogo} />
+                  <div>
+                    <p class="text-white text-xl text-center font-semibold mb-4 text-yellow-300">Kunjungi Sosial Media Kami</p>
+                    <div>
+                      <div class="grid grid-cols-7 mb-2">
+                        <img className="place-self-end col-span-2 mr-2" src={Instagram} />
+                        <div class="place-self-start col-span-5 py-1.5">
+                          <p class="text-white font-semibold pl-2">mongga_indonesia</p>
+                        </div>
+                      </div>
+                      <div class="grid grid-cols-7 mb-2">
+                        <img className="place-self-end col-span-2 mr-2" src={Linkedin} />
+                        <div class="place-self-start col-span-5 py-1.5">
+                          <p class="text-white font-semibold pl-2">Mongga Indonesia</p>
+                        </div>
+                      </div>
+                      <div class="grid grid-cols-7 mb-2">
+                        <img className="place-self-end col-span-2 mr-2" src={Facebook} />
+                        <div class="place-self-start col-span-5 py-1.5">
+                          <p class="text-white font-semibold pl-2">@monggaindonesia</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </>
