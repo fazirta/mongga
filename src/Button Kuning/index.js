324ee8c2e7bd01ea96116aa192dbdc543e5385fd
Fazil <faziltirtana@ctemplar.com>
2021-11-05 15:08:57 +0700

    Fix button kuning

--- src/Button Kuning/index.js
+++ src/Button Kuning/index.js
@@ -3,9 +3,9 @@ import { a } from "react-router-dom";
 export default function ButhrefnKuning(props) {
   return (
     <>
-      <div className="flex justify-center">
+      <div data-aos={props.dataAOS} className="flex justify-center">
         <a href={props.href}>
-          <div className="inline-block bg-yellow-400 rounded-lg font-bold text-darkgreen text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-green-900 font-poppins shadow-2xl">
+          <div className="shadow-lg hover:shadow-xl transition ease-out duration-200 transform hover:-translate-y-0.5 hover:scale-105 inline-block bg-yellow-400 rounded-lg font-bold text-darkgreen text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-yellow-300 font-poppins shadow-2xl">
             {props.text}
           </div>
         </a>
