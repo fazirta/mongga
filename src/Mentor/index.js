1109079aaa05760706d04292349ad86972fb458a
naufala <ahmadajinaufalali@gmail.com>
2021-11-07 17:41:08 +0700

    complete mentor page

--- src/Mentor/index.js
+++ src/Mentor/index.js
@@ -4,6 +4,7 @@ import Heading2 from "../Heading2";
 import Paragraph1 from "../Paragraph1";
 import JumbotronHijau from "../Jumbotron Hijau";
 import MentorItem from "../MentorItem";
+import MentorItem2 from "../MentorItem2";
 import pic_awla from "../img/profile-awla.png";
 import pic_faqihah from "../img/profile-faqihah.png";
 import pic_hilya from "../img/profile-hilya.png";
@@ -29,61 +30,72 @@ export default function Mentor() {
       </div>
 
       <div className="container mx-auto">
-        <div className="flex flex-row justify-around">
-          <div className="flex-col md:w-full">
-            <MentorItem
-              pic={pic_awla}
-              name="Awla Fajri Assalam"
-              bio1="Founder Organisasi Riset Solve-IT ITB"
-              bio2="Wisudawan Terbaik FMIPA ITB 2021"
-            />
-          </div>
-          <div className="flex-col md:w-full">
-            <MentorItem
-              pic={pic_faqihah}
-              name="Faqihah Muharroroh Itsnaini"
-              bio1="Moderator of University through National Scale Events"
-              bio2="Digital Journalist Intern at Detik.com"
-            />
-          </div>
+        <div className="flex flex-col md:flex-row justify-center">
+          <MentorItem
+            pic={pic_awla}
+            name="Awla Fajri Assalam"
+            bio1="Founder Organisasi Riset Solve-IT ITB"
+            bio2="Wisudawan Terbaik FMIPA ITB 2021"
+          />
+          <MentorItem
+            pic={pic_faqihah}
+            name="Faqihah Muharroroh Itsnaini"
+            bio1="Moderator of University through National Scale Events"
+            bio2="Digital Journalist Intern at Detik.com"
+          />
         </div>
-        <div className="flex flex-row pt-24 justify-around">
-          <div className="flex-col">
-            <MentorItem
-              pic={pic_hilya}
-              name="Hilyatushalihah Kholis Audah, BSc"
-              bio1="Founder of Gwel Academy"
-              bio2="First Class Honours Bachelor In Psychology with Neuropsychology of Bangor University"
-            />
-          </div>
-          <div className="flex-col">
-            <MentorItem
-              pic={pic_yudha}
-              name="Maha Yudha Samawi"
-              bio1="Gold Medalist PKM-KC 2020"
-              bio2=""
-            />
-          </div>
+        <div className="flex flex-col md:flex-row md:pt-24 justify-between">
+          <MentorItem
+            pic={pic_hilya}
+            name="Hilyatushalihah Kholis Audah, BSc"
+            bio1="Founder of Gwel Academy"
+            bio2="First Class Honours Bachelor In Psychology with Neuropsychology of Bangor University"
+          />
+          <MentorItem
+            pic={pic_yudha}
+            name="Maha Yudha Samawi"
+            bio1="Gold Medalist PKM-KC 2020"
+            bio2=""
+          />
         </div>
 
-        <div className="flex flex-row pt-24 justify-around">
-          <div className="flex-col">
-            <MentorItem
-              pic={pic_christ}
-              name="Christ Panogari"
-              bio1="President of English Speaking Union"
-              bio2=""
-            />
-          </div>
-          <div className="flex-col">
-            <MentorItem
-              pic={pic_anju}
-              name="Anju Ikhsan"
-              bio1="Gold Medal Olimpiade Sains Indonesia bidang biologi mahasiswa"
-              bio2="Bronze Medal Kompetisi Sains Pengajar IPA Mts & Biologi MA2021 Posi"
-            />
-          </div>
+        <div className="flex flex-col md:flex-row md:pt-24 justify-between">
+          <MentorItem
+            pic={pic_christ}
+            name="Christ Panogari"
+            bio1="President of English Speaking Union"
+            bio2=""
+          />
+          <MentorItem
+            pic={pic_anju}
+            name="Anju Ikhsan"
+            bio1="Gold Medal Olimpiade Sains Indonesia bidang biologi mahasiswa"
+            bio2="Bronze Medal Kompetisi Sains Pengajar IPA Mts & Biologi MA2021 Posi"
+          />
         </div>
+      </div>
+
+      <div className="container mx-auto mt-20">
+        <Heading2
+          value="Beberapa mentor lainnya:"
+          position="text-center mb-10"
+        />
+        <MentorItem2
+          name="Ilham Subandoro"
+          description="Medali perunggu Thailand Innovation Olympiad 2020"
+        />
+        <MentorItem2
+          name="Muhammad Farhan"
+          description="Medali perak KSN tingkat nasional 2018"
+        />
+        <MentorItem2
+          name="Dharma Prasetya Permana"
+          description="Medali perunggu ONMIPA tingkat nasional mahasiswa"
+        />
+        <MentorItem2
+          name="Almira Jannisa"
+          description="Medali perak OSN geografi tingkat nasional 2016"
+        />
       </div>
 
       <div className="my-24 px-3 max-w-7xl mx-auto">
