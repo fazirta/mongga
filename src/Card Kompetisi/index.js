e0cd1711e743d6c178311f8d98b396390cc83c48
Fuad Maulana <fuadmaulana0812@gmail.com>
2021-11-08 20:14:48 +0700

    revisi kompetisi

---
+++ src/Card Kompetisi/index.js
@@ -0,0 +1,26 @@
+export default function CardKompetisi(props) {
+    return (
+        <>
+            <div 
+                className="font-poppins text-xl md:text-2xl font-bold">
+                <div className="flex space-x-5 md:space-x-32 w-full px-48 py-10">
+                    <img
+                        alt=""
+                        className="w-20 h-20 md:w-48 md:h-48 object-cover"
+                        src={props.src}
+                    />
+                    <div className="font-poppins text-left">
+                        <h1
+                            className="text-2xl md:text-4xl font-bold"
+                        >
+                            {props.title}
+                        </h1>
+                        <p className="py-5">
+                            {props.desc}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
